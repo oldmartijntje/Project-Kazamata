ec57ea264c28e21341d7e1d0d5cf6c50f4b8fb42
Martijn <oldmartijntje@gmail.com>
2024-06-01 13:05:46 +0200

    dancing player
    (35:50)

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
 import { resources } from './src/Resource.js';
 import { Sprite } from './src/Sprite.js';
+import { Vector2 } from "./src/vector2";
 
 const canvas = document.querySelector('#game-canvas');
 const ctx = canvas.getContext('2d');
 
-const draw = () => {
-    const sky = resources.images.sky;
-    if (sky.isLoaded) {
-        ctx.drawImage(sky.image, 0, 0);
-    }
-    const ground = resources.images.ground;
-    if (ground.isLoaded) {
-        ctx.drawImage(ground.image, 0, 0);
-    }
-}
+const skySprite = new Sprite({
+    resource: resources.images.sky,
+    frameSize: new Vector2(320, 180),
+});
+
+const groundSprite = new Sprite({
+    resource: resources.images.ground,
+    frameSize: new Vector2(320, 180),
+});
 
 const hero = new Sprite({
     resource: resources.images.hero,
     hFrames: 3,
     vFrames: 8,
     frame: 1,
+    frameSize: new Vector2(32, 32),
 });
 
+const shadow = new Sprite({
+    resource: resources.images.shadow,
+    frameSize: new Vector2(32, 32),
+});
+
+const heroPos = new Vector2(16 * 6, 16 * 5);
+
+const draw = () => {
+    skySprite.drawImage(ctx, 0, 0);
+    groundSprite.drawImage(ctx, 0, 0);
+
+    // center the hero
+    const heroOffset = new Vector2(-8, -21);
+    const heroPosX = heroPos.x + heroOffset.x;
+    const heroPosY = heroPos.y + 1 + heroOffset.y;
+
+    shadow.drawImage(ctx, heroPosX, heroPosY);
+    hero.drawImage(ctx, heroPosX, heroPosY);
+}
+
+
 setInterval(() => {
+    hero.frame = (hero.frame + 1) % 24;
     draw();
 
 }, 300)
--- src/Sprite.js
+++ src/Sprite.js
@@ -1,3 +1,5 @@
+import { Vector2 } from "./vector2";
+
 export class Sprite {
     constructor({
         resource, // image resource
@@ -9,13 +11,13 @@ export class Sprite {
         position // position of the sprite
     }) {
         this.resource = resource;
-        this.frameSize = frameSize;
+        this.frameSize = frameSize ?? new Vector2(16, 16);
         this.hFrames = hFrames ?? 1;
         this.vFrames = vFrames ?? 1;
         this.frame = frame ?? 0;
         this.frameMap = new Map();
         this.scale = scale ?? 1;
-        this.position = position;
+        this.position = position ?? new Vector2(0, 0);
         this.buildFrameMap();
     }
 
@@ -23,13 +25,45 @@ export class Sprite {
         let frameCount = 0;
         for (let y = 0; y < this.vFrames; y++) {
             for (let x = 0; x < this.hFrames; x++) {
-                console.log(x, y);
-                this.frameMap.set(frameCount, {
-                    x: x * this.frameSize.width,
-                    y: y * this.frameSize.height
-                });
+                this.frameMap.set(
+                    frameCount,
+                    new Vector2(
+                        x * this.frameSize.x,
+                        y * this.frameSize.y
+                    )
+                );
                 frameCount++;
             }
         }
+    }
+
+    drawImage(ctx, x, y) {
+        if (!this.resource.isLoaded) {
+            return;
+        }
+
+        // find the correct sprite sheet frame to use
+        let frameCordX = 0
+        let frameCordY = 0
+        const frame = this.frameMap.get(this.frame);
+        if (frame) {
+            frameCordX = frame.x;
+            frameCordY = frame.y;
+        }
+
+        const frameSizeX = this.frameSize.x;
+        const frameSizeY = this.frameSize.y;
+
+        ctx.drawImage(
+            this.resource.image,
+            frameCordX,
+            frameCordY,
+            frameSizeX,
+            frameSizeY,
+            x,
+            y,
+            frameSizeX * this.scale,
+            frameSizeY * this.scale
+        );
     }
 }
