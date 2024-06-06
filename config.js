70d05f9a048b7b0a82a4b6b5d95fb0aa66c0ddc1
Martijn <oldmartijntje@gmail.com>
2024-06-06 15:51:08 +0200

    trying to see why sprites aren't working

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
     "baseUrl": "Project-Kazamata",
-    "publicPath": "",
+    "publicPath": "public",
     "keys": {
         "upKeys": ["ArrowUp", "w"],
         "downKeys": ["ArrowDown", "s"],
